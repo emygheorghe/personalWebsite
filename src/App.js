c15ff917ae198fe2b2fe0cf40310e2b0d4e61ce1
Emil Gheorghe <emi.c.gheorghe@gmail.com>
2025-07-31 21:24:54 +0100

    Add coming soon message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Welcome to My Personal Website</h1>
+        <p>This React app is now running on GitHub Pages 🎉</p>
       </header>
+
     </div>
   );
 }
